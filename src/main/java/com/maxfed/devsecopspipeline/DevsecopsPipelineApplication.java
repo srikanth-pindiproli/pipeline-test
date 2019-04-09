a9c7172d17a50cad7250ddb688d66db0ab7219f3
Satya Srikanth Pindiproli <satyaspindiproli@maximus.com>
2019-04-09 12:59:49 -0400

    Renamed application. Testing to see if GitHub will trigger Jenkins jobs.

---
+++ src/main/java/com/maxfed/devsecopspipeline/DevsecopsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.maxfed.devsecopspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecopsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsecopsPipelineApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/maxfed/devsecopspipeline/controller/HelloController.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.maxfed.devsecopspipeline.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author srikanth.maximus
+ *
+ */
+@RestController()
+@RequestMapping("/hello")
+public class HelloController {
+
+  @GetMapping("/")
+  public ResponseEntity<String> hello() {
+    return new ResponseEntity<String>("Hello World", HttpStatus.OK);
+  }
+
+  @GetMapping("/{guestName}")
+  public ResponseEntity<String> hello(@PathVariable String guestName) {
+    return new ResponseEntity<String>("Hello " + guestName + "!", HttpStatus.OK);
+  }
+}
